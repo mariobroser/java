abc534a8acd92fa53926a51ce619f6ce3fa21d21
unknown <erbolatsuleev@mail.ru>
2023-02-26 00:56:08 +0500

    first

---
+++ homework001.java
@@ -0,0 +1,21 @@
+public class homework001 {
+    public static void main(String[] args){
+
+    }
+    
+
+
+void print(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+        System.out.println(array[i]);
+    }
+}
+
+int summE1(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+}
